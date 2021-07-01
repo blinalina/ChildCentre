879ec981ee4989e8c0582f19f8fdd0a8645a446e
Алина Гусева <avguseva2002@gmail.com>
2021-07-02 01:49:05 +0300

    Создала класс для подключения к базе данных. Создала в базе данных таблицы аккаунтов и ролей. Реализовала метод авторизации и классы исключений для различных ситуаций при авторизации. Добавила кеширование id и Роли пользователя.

--- ChildCentre/AuthorizationForm.Designer.cs
+++ ChildCentre/AuthorizationForm.Designer.cs
@@ -29,13 +29,34 @@ namespace ChildCentre
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
